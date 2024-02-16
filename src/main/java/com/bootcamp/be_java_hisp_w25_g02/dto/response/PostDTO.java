8431d252cb82f95441d8b8c06e7f40d268fac7f4
nick-urzagasti <nicolas.urzagasti@mercadolibre.com>
2024-02-16 17:01:04 -0300

    Creados DTO

---
+++ src/main/java/com/bootcamp/be_java_hisp_w25_g02/dto/response/PostDTO.java
@@ -0,0 +1,11 @@
+package com.bootcamp.be_java_hisp_w25_g02.dto.response;
+
+import java.time.LocalDate;
+
+public record PostDTO(
+        Long user_id,
+        LocalDate date,
+        ProductDTO product,
+        Integer category,
+        Double price
+) { }
--- src/main/java/com/bootcamp/be_java_hisp_w25_g02/repository/PostRepositoryImpl.java
+++ src/main/java/com/bootcamp/be_java_hisp_w25_g02/repository/PostRepositoryImpl.java
@@ -14,15 +14,15 @@ public class PostRepositoryImpl {
     List<Post> postList = new ArrayList<>(List.of(
             new Post(0L,
                     new User(0L, "Juan", true, List.of(
-                            new User(1L, "Pepe", false, null, null)), null), LocalDate.of(2000, 01, 01),
+                            new User(1L, "Pepe", true, null, null)), null), LocalDate.of(2000, 01, 01),
                     new Product(0, "Pelopincho", "Piletas", "XXX", "Azul", "2000 litros"), 2, 52000.0),
             new Post(1L,
                     new User(2L, "John", true, List.of(
-                            new User(3L, "Juana", false, null, null)), null), LocalDate.of(2005, 02, 10),
+                            new User(3L, "Juana", true, null, null)), null), LocalDate.of(2005, 02, 10),
                     new Product(0, "Lentes de sol", "Lentes", "Ray Ban", "Gris", "Lorem ipsum"), 3, 13500.0),
             new Post(2L,
                     new User(4L, "Martin", true, List.of(
-                            new User(5L, "Ariel", false, null, null)), null), LocalDate.of(2005, 02, 10),
+                            new User(5L, "Ariel", true, null, null)), null), LocalDate.of(2005, 02, 10),
                     new Product(0, "Mouse gamer", "Informática", "Logitech", "Gris", "Lorem ipsum"), 4, 7500.0)
     ));
 }
