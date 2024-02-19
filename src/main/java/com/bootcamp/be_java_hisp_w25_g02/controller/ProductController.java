145c7a0b0a4e80c0052ed4ead27b6ea5a5c88761
nico-aimar <mario.aimar@mercadolibre.com>
2024-02-19 14:46:49 -0300

    refactors

--- src/main/java/com/bootcamp/be_java_hisp_w25_g02/controller/ProductController.java
+++ src/main/java/com/bootcamp/be_java_hisp_w25_g02/controller/ProductController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class ProductController {
 
     private IPostService postService;
