0fbecf22c52a7cf095417c23c724764023793226
nico-aimar <mario.aimar@mercadolibre.com>
2024-02-19 14:47:36 -0300

    refactors

--- src/main/java/com/bootcamp/be_java_hisp_w25_g02/controller/ProductController.java
+++ src/main/java/com/bootcamp/be_java_hisp_w25_g02/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
 
-    private IPostService postService;
+    private final IPostService postService;
     public ProductController(PostServiceImpl postService){
         this.postService = postService;
     }
