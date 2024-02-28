1175e8b872c199787bd5b565215ee088c2182001
Arrigoz <arrigo.zanabonidibatista@mercadolibre.com>
2024-02-28 15:04:07 -0300

    cambios en 007

--- src/test/java/com/bootcamp/be_java_hisp_w25_g02/controller/UserControllerCountFollowersTest.java
+++ src/test/java/com/bootcamp/be_java_hisp_w25_g02/controller/UserControllerCountFollowersTest.java
@@ -1,48 +1,45 @@
 package com.bootcamp.be_java_hisp_w25_g02.controller;
 
 import com.bootcamp.be_java_hisp_w25_g02.dto.response.FollowerCountDTO;
-import com.bootcamp.be_java_hisp_w25_g02.repository.IUserRepository;
 import com.bootcamp.be_java_hisp_w25_g02.service.IUserService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
 import java.util.Objects;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserControllerCountFollowersTest {
     @Mock
-    IUserService iUserService;
+    IUserService userService;
     @InjectMocks
     UserController userController;
     @Test
     void getUserTotalFollowers() throws Exception {
         //Arrange - llama elementos a usar
         FollowerCountDTO expected = new FollowerCountDTO(1, "Javier", 2L);
-        when(iUserService.getUserTotalFollowers(1)).thenReturn(expected);
+        when(userService.getUserTotalFollowers(1)).thenReturn(expected);
         //Act - ejecuta el método a testear
         ResponseEntity<FollowerCountDTO> current = userController.getUserTotalFollowers(1);
         //Assert - verifica que el resultado sea lo esperado
-        System.out.println(expected.followersCount());
-        System.out.println(Objects.requireNonNull(current.getBody()).followersCount());
-        assertThat(expected.followersCount()).isEqualTo(Objects.requireNonNull(current.getBody()).followersCount());
+        assertThat(current.getStatusCode()).isEqualTo(HttpStatus.OK);
+        FollowerCountDTO actual = current.getBody();
+        assertThat(actual).isEqualTo(expected);
     }
 
-    /*
     @Test
+    @Disabled
     void getUserTotalFollowersCountPath() throws Exception {
         //Arrange - llama elementos a usar
         Long expected = 2L;
-        when(iUserService.getUserTotalFollowers(1).followersCount()).thenReturn(expected);
+        when(userService.getUserTotalFollowers(1).followersCount()).thenReturn(expected);
         //Act - ejecuta el método a testear
         ResponseEntity<FollowerCountDTO> current = userController.getUserTotalFollowers(1);
         //Assert - verifica que el resultado sea lo esperado
@@ -50,5 +47,4 @@ class UserControllerCountFollowersTest {
         System.out.println(current);
         assertThat(expected).isEqualTo(current);
     }
-    */
 }
