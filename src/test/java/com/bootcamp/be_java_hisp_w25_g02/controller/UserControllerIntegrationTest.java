7f827518771934deb0be1b53086b607c64d77e3e
nick-urzagasti <nicolas.urzagasti@mercadolibre.com>
2024-02-28 16:33:14 -0300

    added user controller integration test

---
+++ src/test/java/com/bootcamp/be_java_hisp_w25_g02/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.bootcamp.be_java_hisp_w25_g02.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserControllerIntegrationTest {
+
+}
